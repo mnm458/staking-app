7189693ca5b5146a17579a811f5ad8c4e9c95929
mnm458 <mohaknainmalhotra@gmail.com>
2022-12-18 12:57:28 +1100

    Vault added to deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,10 @@
 import { ethers } from "ethers";
 import "dotenv/config";
-import * as vaultJson from "../artifacts/contracts/Vault.sol/Vault.json"
+import * as vaultJson from "../artifacts/contracts/Vault.sol/Vault.json";
 
 const EXPOSED_KEY = "loremipsum";
+const CETH_ADDR = "0x64078a6189Bf45f80091c6Ff2fCEe1B15Ac8dbde";
+const COMPTROLLER_ADDR = "0x05Df6C772A563FfB37fD3E04C1A279Fb30228621";
 
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
@@ -23,6 +25,26 @@ async function main() {
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));
   console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+  console.log("Deploying Vault contract");
+  
+  const vaultFactory = new ethers.ContractFactory(
+    vaultJson.abi,
+    vaultJson.bytecode,
+    signer
+  );
+  const vaultContract = await vaultFactory.deploy(
+    convertStringArrayToBytes32(proposals),
+    ethers.utils.getAddress(CETH_ADDR),
+    ethers.utils.getAddress(COMPTROLLER_ADDR)
+
+  );
+  console.log("Awaiting confirmations");
+  await vaultContract.deployed();
+  console.log("Completed");
+  console.log(`Contract deployed at ${vaultContract.address}`);
 }
 
 main().catch((error) => {
