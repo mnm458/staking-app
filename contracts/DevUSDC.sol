66a6d93ca35043df57ec9a5b2e33689e2a967b0a
mnm458 <mohaknainmalhotra@gmail.com>
2022-12-19 01:26:16 +1100

    DevUSDC contract changes

--- contracts/DevUSDC.sol
+++ contracts/DevUSDC.sol
@@ -3,15 +3,20 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract DevUSDC is ERC20, Ownable, ERC20Permit{
+contract DevUSDC is ERC20, AccessControl, ERC20Permit{
+      bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+      constructor() 
+      ERC20("DevUSDC", "DUSDC") ERC20Permit("DevUSDC") {
+         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+      }
 
 
-      constructor() ERC20("DevUSDC", "DUSDC") ERC20Permit("DevUSDC") {}
-
-      function mint(address to, uint256 amount) public onlyOwner {
+      function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
