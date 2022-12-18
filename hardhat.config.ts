e54efbde3c3be64ff643f002a07f5a3c650831bb
mnm458 <mohaknainmalhotra@gmail.com>
2022-12-18 12:32:08 +1100

    Added config to Hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  paths: { tests: "test" },
+  networks: {
+    goerli: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
